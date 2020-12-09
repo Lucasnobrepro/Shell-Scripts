bd38ea9266953f49cf5ce50281ecc24a50f3b8bd
Lucas Nobre <lucasnobrepro2015@gmail.com>
2020-12-08 21:55:09 -0300

    ex2

---
+++ exercicios/exercicios02/parte_01.sh
@@ -0,0 +1,4 @@
+grep -E '[[:digit:]]{6}[[:space:]]*A' /home/compartilhado/emailsordenados.txt
+grep -E '\bA' /home/compartilhado/emailsordenados.txt
+grep -E '.br$' /home/compartilhado/emailsordenados.txt
+grep -E '[[:alpha:]]*[[:digit:]]+[[:alpha:]]*@' /home/compartilhado/emailsordenados.txt
---
+++ exercicios/exercicios02/parte_02.sh
@@ -0,0 +1,11 @@
+
+#1. A palavra ”revista”no singular e no plural.
+'revist(a|as)'
+
+#2. A palavra ”letra”em qualquer combinacao de letras maiusculas ou minusculas.
+'(L|l)(E|e)(T|t)(R|r)(A|a)'
+
+#3. Os numeros inteiros.
+'(+|-)[[:digit:]]+[[:space:]]'
+
+
