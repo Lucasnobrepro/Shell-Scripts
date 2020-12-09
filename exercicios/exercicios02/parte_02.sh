c3cf9941af8a708793dbd955763e4733c7dbe3f3
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-09 07:47:37 -0300

    Update parte_02.sh
    Correção.

--- exercicios/exercicios02/parte_02.sh
+++ exercicios/exercicios02/parte_02.sh
@@ -1,5 +1,6 @@
 
 #1. A palavra ”revista”no singular e no plural.
+# O melhor seria revistas? ou revista(|s) 
 'revist(a|as)'
 
 #2. A palavra ”letra”em qualquer combinacao de letras maiusculas ou minusculas.
