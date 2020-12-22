ae0aba51255fc8677a9cbbfa117a2c2e02eddf60
lucasnobrepro <lucasnobrepro2015@gmail.com>
2020-12-21 22:23:38 -0300

    second question

---
+++ atividades/atividades04/codigo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sed -Ei 's:/bin/python:/usr/bin/python3:' /home/compartilhado/atividade04.py
+sed -Ei 's:nota1:NOTA1: ; s:nota2:NOTA2: ; s:notaFinal:NOTAFINAL:' /home/compartilhado/atividade04.py
+sed -Ei '3a \import time' /home/compartilhado/atividade04.py
+sed -Ei '$a \   time.ctime() ' /home/compartilhado/atividade04.py
