e4d6241d8ca719bb5efca49a3421bcc3bf51e228
Lucas Nobre <lucasnobrepro2015@gmail.com>
2021-01-19 21:02:16 -0300

    execicio06

--- exercicios/exercicios06/auxExec.sh
+++ exercicios/exercicios06/auxExec.sh
@@ -2,7 +2,7 @@
 
 endereco="$1"
 echo "INICIO" >> "$endereco.txt"
-for i in $(seq 0 1 20); do
+for i in $(seq 0 1 255); do
     ping -c 1 -q -w 1 "$endereco.$i" >> logs.txt
     if [ $? == 0 ]; then
         echo "$endereco.$i on" >> "$endereco.txt"    
