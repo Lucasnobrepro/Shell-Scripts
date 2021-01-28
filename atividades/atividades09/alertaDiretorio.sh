053fc5a6ec225fdacbc865b203225d74e82a23fb
Lucas Nobre <lucasnobrepro2015@gmail.com>
2021-01-27 22:12:35 -0300

    atividades09

---
+++ atividades/atividades09/alertaDiretorio.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+time_space="$1"
+dir="$2"
+
+vetor=()
+add=()
+delete=()
+
+nFile="$(ls $dir| wc -l)"
+
+[ -d cache ]
+if [ $? != 0 ]; then
+    mkdir cache
+fi
+
+echo $(ls $dir) > "cache/old.log"
+
+while true 
+do 
+    mFile="$(ls $dir| wc -l)"
+    if [ $mFile -gt $nFile ]; then
+        echo $(ls $dir) > "cache/add.log"
+        for i in $(cat cache/old.log); do
+            vetor+=($i)
+        done
+
+        for z in $(cat cache/add.log); do
+            if [[ ! " ${vetor[@]} " =~ " $z " ]]; then
+                add+=($z)  
+            fi
+        done
+        echo $(date +"%d-%m-%y %T") " Alteração! $nFile->$mFile Adicionados ${add[*]}">> dirSensors.log
+        echo $(ls $dir) > "cache/old.log"
+        nFile=$mFile
+        add=()
+
+    elif [ $mFile -lt $nFile ]; then
+        echo $(ls $dir) > "cache/add.log"
+        for r in $(cat cache/old.log); do
+            [ ! -f $dir/$r ]
+            if [ $? == 0 ]; then
+                delete+=($r)
+            fi
+        done
+        echo $(date +"%d-%m-%y %T") " Alteração! $nFile->$mFile Removidos  ${delete[*]}">> dirSensors.log
+        echo $(ls $dir) > "cache/old.log"
+        delete=()
+        nFile=$mFile
+    fi
+    sleep $time_space
+done
