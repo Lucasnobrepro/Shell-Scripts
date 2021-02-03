4934753d0a057a0c9537e4d5373104bc92c4240e
Lucas Nobre <lucasnobrepro2015@gmail.com>
2021-02-03 12:30:24 -0300

    atividade10

--- atividades/atividades10/contaPalavras.sh
+++ atividades/atividades10/contaPalavras.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+file=$1
+one=
+# read -p "Informe o arquivo:" file
+
+declare -A text
+
+for word in $(cat $file); do
+    # echo $($text[$word])
+    if [  "${text[$word]}" == "" ]; then
+        text["$word"]=1
+    else
+        text["$word"]=`expr ${text[$word]} + 1`
+    fi
+
+done
+
+for i in ${!text[@]}; do
+    echo "$i : ${text[$i]}"
+done
