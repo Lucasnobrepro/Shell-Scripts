aebbd0db98097de3ccd3efcdc50ddf820c17095d
Lucas Nobre <lucasnobrepro2015@gmail.com>
2021-02-23 21:36:33 -0300

    ex11

---
+++ exercicios/exercicios11/criarInstancia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SUBNET=$(aws ec2 describe-subnets --query "Subnets[0].SubnetId" --output text)
+IMAGE="ami-03d315ad33b9d49c4"
+KEY=$1
+SGI=$(aws ec2 describe-security-groups --query "SecurityGroups[0].GroupId" --output text)
+
+echo "Criando instacia!"
+aws ec2 run-instances --image-id $IMAGE --instance-type "t2.micro" --key-name $KEY --security-group-ids $SGI --subnet-id $SUBNET
+
+IPPUBLIC=$(aws ec2 describe-instances --query "Reservations[1].Instances[0].PublicIpAddress" --output text)
+
+echo "IP PUBLICO: $IPPUBLIC"
