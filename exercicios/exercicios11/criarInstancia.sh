fb1fd501992dda588863e674591350b2fe6fec9f
Lucas Nobre <lucasnobrepro2015@gmail.com>
2021-02-23 21:54:31 -0300

    ex11

--- exercicios/exercicios11/criarInstancia.sh
+++ exercicios/exercicios11/criarInstancia.sh
@@ -2,7 +2,9 @@
 SUBNET=$(aws ec2 describe-subnets --query "Subnets[0].SubnetId" --output text)
 IMAGE="ami-03d315ad33b9d49c4"
 KEY=$1
-SGI=$(aws ec2 describe-security-groups --query "SecurityGroups[0].GroupId" --output text)
+VPCID=$(aws ec2 describe-instances --query "Reservations[1].Instances[0].VpcId" --output text)
+SGI=$(aws ec2 create-security-group --group-name MySecurityGroup --description "My security group" --vpc-id $VPCID --output text)
+#SGI=$(aws ec2 describe-security-groups --query "SecurityGroups[0].GroupId" --output text)
 
 echo "Criando instacia!"
 aws ec2 run-instances --image-id $IMAGE --instance-type "t2.micro" --key-name $KEY --security-group-ids $SGI --subnet-id $SUBNET
